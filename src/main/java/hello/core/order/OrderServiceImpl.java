16066a9b51a05e0c7a141d78b1dacf137c21edeb
ssibongee <ssibongee@gmail.com>
2020-09-23 21:12:03 +0900

    주문 서비스 구현체 생성

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // SRP가 잘 적용된 예시 OrderService에서 할인과 관련된 변경에 상관이없다.
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
