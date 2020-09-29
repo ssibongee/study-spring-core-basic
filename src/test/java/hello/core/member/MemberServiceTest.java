7e4a8578b8acdcc8e817fb00028ddb2c74d3ac2c
ssibongee <ssibongee@gmail.com>
2020-09-30 01:51:55 +0900

    테스트 코드에 대해서도 DIP와 OCP 원칙을 지키도록 수정한 코드를 적용

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,21 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    // 테스트를 실행하기 전에 수행
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
 
     @Test
     void join() {
