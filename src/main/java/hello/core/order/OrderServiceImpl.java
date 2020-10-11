d9fcc3c12a58beb15ccfc9ddc39151f7e90c9783
ssibongee <ssibongee@gmail.com>
2020-10-11 17:59:41 +0900

    중복된 타입의 빈이 존재할 때 처리하는 다양한 방법 테스트

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,15 +8,21 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final 키워드가 붙은 변수를 파라미터로 받는 생성자를 만들어준다.
+//@RequiredArgsConstructor // final 키워드가 붙은 변수를 파라미터로 받는 생성자를 만들어준다.
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
