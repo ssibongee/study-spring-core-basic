da950fd6f3fb26ce920800a628a59a555cdb7320
안시원 <ssibongee@nts-corp.com>
2020-11-29 04:30:33 +0900

    싱글톤 빈 테스트

---
+++ src/test/java/scope/SingletonTest.java
@@ -0,0 +1,47 @@
+package scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.util.Assert;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    public void singletonBeanFind() throws Exception {
+        // given
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean singletonBean1 = context.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = context.getBean(SingletonBean.class);
+        // when
+
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        assertThat(singletonBean1).isEqualTo(singletonBean2);
+
+        // then
+
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("SingletonBean.close");
+        }
+    }
+}
